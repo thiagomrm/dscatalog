e5939ddede408e6fdd7c242ad36e03c127e4c86c
thiagomrm <93530961+thiagomrm@users.noreply.github.com>
2022-02-28 18:49:10 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO{
+	private static final long serialVersionUID = 1L;
+
+	private String password;
+	
+	UserUpdateDTO(){
+		super();
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
