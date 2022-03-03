dbea7fbfc4e132b8da855d1c5b4ca46767873285
thiagomrm <93530961+thiagomrm@users.noreply.github.com>
2022-03-03 15:58:47 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -5,19 +5,5 @@ import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
 @UserUpdateValid
 public class UserUpdateDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
-
-	private String password;
-	
-	UserUpdateDTO(){
-		super();
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	
 }
