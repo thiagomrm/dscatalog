c4c48075d0404ccf631a4813d6f84585dc9c27a8
thiagomrm <93530961+thiagomrm@users.noreply.github.com>
2022-04-19 14:15:30 -0300

    useState, useEffect, useParams

--- frontweb/src/pages/ProductsDetails/index.tsx
+++ frontweb/src/pages/ProductsDetails/index.tsx
@@ -1,10 +1,30 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
+type UrlParams = {
+  productId: string;
+}
+
 const ProductDetails = () => {
+  
+  const { productId } = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then(response => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -18,23 +38,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome do produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do Produto</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Repudiandae esse, ipsa aut culpa, laudantium numquam eligendi
-                consequatur, cumque quae beatae eum at dicta? Nulla, placeat vel
-                a quibusdam ex libero?
+                {product?.description}
               </p>
             </div>
           </div>
